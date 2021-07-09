ad08ce9775c150311088c2dcb2c44f3bcda1df83
HeikoKramer <github@myforce.net>
2021-07-09 18:05:20 +0200

    folder structure changed

--- demos/edgeledger_website/js/main.js
+++ demos/edgeledger_website/js/main.js
@@ -21,7 +21,7 @@ window.addEventListener('scroll', function() {
 })
 
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function(event) {
+$('#navbar a').on('click', function(event) {
   if (this.hash !== '') {
     event.preventDefault();
 
