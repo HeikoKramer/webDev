0f67b8ecf5814ce8008a48a28a352a7c4814b52d
HeikoKramer <github@myforce.net>
2021-08-01 18:56:11 +0200

    data type examples added

--- javascript_notes_2.md
+++ javascript_notes_2.md
@@ -267,4 +267,51 @@ console.log(numbers);
 * most other languages are statically typed (Java, C#, C++)
 * there are supersets of JS and addons to allow static typing (TypeScript, Flow)
 
+## typeof
+`typeof` provides a way to verify the data type of a variable. <br>
+**NOTE:** A bit weird is the fact that `typeof null === 'object';`. <br>
+Null is a primitive data type and it should look like this: `typeof null === 'null'` <br>
+That is some historical issue that I don't really care about, but if you're interested, ready the [typeof null explanations](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#typeof_null). <br>
+**Reference Types** will all come back as **objects**. <br>
 
+```js
+// PRIMITIVE
+console.log("– P R I M I T I V E –");
+// String
+const name = "John Doe";
+console.log("datatype of name: " + typeof name);
+// Number
+const age = 45;
+const quotedAge = "45"; // <-- quotes around numbers convert them into strings
+console.log("datatype of age: " + typeof age);
+console.log("datatype of quotedAge: " + typeof quotedAge);
+// Boolean
+const hasKids = true;
+console.log("datatype of hasKids: " + typeof hasKids);
+// Null
+const car = null;
+console.log("datatype of car: " + typeof car);
+// Undefined
+let test;
+console.log("datatype of test: " + typeof test);
+// Symbol
+const sym = Symbol();
+console.log("datatype of sym: " + typeof sym);
+
+// REFERENCE TYPES
+console.log("– R E F E R E N C E –");
+// Array
+const hobbies = ["movies", "music"];
+console.log("datatype of hobbies: " + typeof hobbies);
+// Object literal
+const address = {
+  city: "Boston",
+  state: "MA",
+};
+console.log("datatype of address: " + typeof address);
+// Date
+const today = new Date();
+console.log("datatype of today: " + typeof today);
+```
+
+![data types](/images/datatypes.png)
--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,50 +1,37 @@
-// // var, let const
+// PRIMITIVE
+console.log("– P R I M I T I V E –");
+// String
+const name = "John Doe";
+console.log("datatype of name: " + typeof name);
+// Number
+const age = 45;
+const quotedAge = "45"; // <-- quotes around numbers convert them into strings
+console.log("datatype of age: " + typeof age);
+console.log("datatype of quotedAge: " + typeof quotedAge);
+// Boolean
+const hasKids = true;
+console.log("datatype of hasKids: " + typeof hasKids);
+// Null
+const car = null;
+console.log("datatype of car: " + typeof car);
+// Undefined
+let test;
+console.log("datatype of test: " + typeof test);
+// Symbol
+const sym = Symbol();
+console.log("datatype of sym: " + typeof sym);
 
-// var name = "John Doe";
-// console.log(name);
-// var name = "Steve Smith";
-// console.log(name);
-
-// // Init var
-// var greeting;
-// console.log(greeting);
-// greeting = "Hello";
-// console.log(greeting);
-
-// // letters, numbers, _, $
-// // Can not start with number
-// var 1up = 'Mario';
-
-// var firstName = 'John'; // Camel Case  <-- use this!
-
-// var first_name ='Sara'; // Underscore (common in PHP)
-// var FirstName = 'Tom'; // Pascal case
-// var firstname;
-
-// // LET
-// let name;
-// name = "John Doe";
-// console.log(name);
-// // name = "Steve Smith";
-// // console.log(name);
-
-// CONST
-// const name = "John";
-// console.log(name);
-// // Can not Re-assign
-// name = "Sarah";
-// const greeting;
-
-// const person = {
-//   name: "John",
-//   age: 30,
-// };
-// console.log(person);
-
-// person.name = "Sara";
-// person.age = 32;
-// console.log(person);
-
-const numbers = [1, 2, 3, 4, 5];
-numbers = [1, 2, 3, 4, 5, 6];
-console.log(numbers);
+// REFERENCE TYPES
+console.log("– R E F E R E N C E –");
+// Array
+const hobbies = ["movies", "music"];
+console.log("datatype of hobbies: " + typeof hobbies);
+// Object literal
+const address = {
+  city: "Boston",
+  state: "MA",
+};
+console.log("datatype of address: " + typeof address);
+// Date
+const today = new Date();
+console.log("datatype of today: " + typeof today);
