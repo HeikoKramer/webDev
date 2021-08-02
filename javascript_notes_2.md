e5d0ef2741b52d2a0e8a0dbc350016b08812f25e
HeikoKramer <github@myforce.net>
2021-08-02 22:27:32 +0200

    simple math added

--- javascript_notes_2.md
+++ javascript_notes_2.md
@@ -565,3 +565,31 @@ console.log(typeof sum);
 ```
 
 ![coercion](/images/coercion.png)
+
+## Numbers & the math object
+### // Simple math with numbers
+
+```js
+let num1 = 100;
+let num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+console.log(val);
+
+val = num1 * num2;
+console.log(val);
+
+val = num1 - num2;
+console.log(val);
+
+val = num1 / num2;
+console.log(val);
+
+num2 = 60
+val = num1 % num2;
+console.log(val);
+```
+
+![simple math](/images/simple_math.png)
--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,117 +1,20 @@
-// let num = 123;
-// let bou = false;
-// let dat = new Date();
-// let arr = [1, 2, 3, 4];
+let num1 = 100;
+let num2 = 50;
+let val;
 
-// console.log("– tobouing() cenversion –");
+// Simple math with numbers
+val = num1 + num2;
+console.log(val);
 
-// // Number to bouing
-// console.log("– Number to bouin –");
-// num = num.tobouing();
-// // Output
-// console.log("num: " + num);
-// console.log("num: " + typeof num);
-// console.log("num: " + num.length);
+val = num1 * num2;
+console.log(val);
 
-// // Boulean to string
-// console.log("– Boulean to strin –");
-// bou = bou.toString();
-// // Output
-// console.log("bou: " + bou);
-// console.log("bou: " + typeof bou);
-// console.log("bou: " + bou.length);
+val = num1 - num2;
+console.log(val);
 
-// // Date to string
-// console.log("– Date to strin –");
-// dat = dat.toString();
-// // Output
-// console.log("dat: " + dat);
-// console.log("dat: " + typeof dat);
-// console.log("dat: " + dat.length);
+val = num1 / num2;
+console.log(val);
 
-// // Array to string
-// console.log("– Array to strin –");
-// arr = arr.toString();
-// // Output
-// console.log("arr: " + arr);
-// console.log("arr: " + typeof arr);
-// console.log("arr: " + arr.length);
-
-// String to number
-// console.log("– String (number) to number –");
-// nuStr = Number("5");
-
-// // Output
-// console.log("nuStr: " + nuStr);
-// console.log("nuStr: " + typeof nuStr);
-// console.log("nuStr: " + nuStr.toFixed(2));
-
-// // String to number
-// console.log("– String (text) to number –");
-// texStr = Number("Gude!");
-
-// // Output
-// console.log("texStr: " + texStr);
-// console.log("texStr: " + typeof texStr);
-// console.log("texStr: " + texStr.toFixed(2));
-
-// // Boolean to number
-// console.log("– Boolean (true) to number –");
-// bouTrue = Number(true);
-
-// // Output
-// console.log("bouTrue: " + bouTrue);
-// console.log("bouTrue: " + typeof bouTrue);
-// console.log("bouTrue: " + bouTrue.toFixed(2));
-
-// // Boolean to number
-// console.log("– Boolean (false) to number –");
-// bouFalse = Number(false);
-
-// // Output
-// console.log("bouFalse: " + bouFalse);
-// console.log("bouFalse: " + typeof bouFalse);
-// console.log("bouFalse: " + bouFalse.toFixed(2));
-
-// // null to number
-// console.log("– null to number –");
-// nul = Number(null);
-
-// // Output
-// console.log("nul: " + nul);
-// console.log("nul: " + typeof nul);
-// console.log("nul: " + nul.toFixed(2));
-
-// // Array to number
-// console.log("– Array to number –");
-// arr = Number([1, 2, 3]);
-
-// // Output
-// console.log("arr: " + arr);
-// console.log("arr: " + typeof arr);
-// console.log("arr: " + arr.toFixed(2));
-
-// // parseInt
-// console.log("– parseInt –");
-// int = parseInt("100.3");
-
-// // Output
-// console.log("int: " + int);
-// console.log("int: " + typeof int);
-// console.log("int: " + int.toFixed(2));
-
-// // parseFloat
-// console.log("– parseFloat –");
-// float = parseFloat("100.3");
-
-// // Output
-// console.log("float: " + float);
-// console.log("float: " + typeof float);
-// console.log("float: " + float.toFixed(2));
-
-const value1 = 5;
-const value2 = "9";
-let sum = value1 + value2;
-
-console.log(sum);
-console.log(typeof sum);
+num2 = 60;
+val = num1 % num2;
+console.log(val);
