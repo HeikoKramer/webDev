3d4cdc9496d2e37c252dceca6474d9028042e9da
HeikoKramer <github@myforce.net>
2021-08-08 22:00:56 +0200

    day picker switch added#

--- javascript_notes_2.md
+++ javascript_notes_2.md
@@ -1590,3 +1590,37 @@ switch (color) {
 // > Color is blue
 ```
 
+Here an other example for a the use of **switch statements** – here we are getting the day of the week, depending on what index the `.getDay()` function of the `Date()` object will give us. <br>
+
+```js
+let day;
+
+// Get the Day of the Week
+// Index starts with 0 for "Sunday"
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+}
+
+console.log(day);
+```
--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,13 +1,44 @@
-const color = "red";
+const color = "blue";
 
 switch (color) {
   case "red":
     console.log("Color is red");
-  // break;
+    break;
   case "blue":
     console.log("Color is blue");
-  // break;
+    break;
   default:
     console.log("Color is not red or blue");
     break;
 }
+
+let day;
+
+// Get the Day of the Week
+// Index starts with 0 for "Sunday"
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+}
+
+console.log(day);
