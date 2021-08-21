f5315b7620a549652a54f2080ec2bc16ab978edd
HeikoKramer <github@myforce.net>
2021-08-21 21:33:12 +0200

    ui vars added

---
+++ js_tutorials/DOM_Projects/taskList/app.js
@@ -0,0 +1,6 @@
+// Define UI Variables
+const form      = document.querySelector('#task-form');
+const taskList  = document.querySelector('.collection');
+const clearBtn  = document.querySelector('.clear-tasks');
+const filter    = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
