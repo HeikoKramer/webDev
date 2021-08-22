32be8d0f47e1f35374d3dfce215dfada4fcdd97f
HeikoKramer <github@myforce.net>
2021-08-22 22:44:21 +0200

    task filter added

--- js_tutorials/DOM_Projects/taskList/app.js
+++ js_tutorials/DOM_Projects/taskList/app.js
@@ -15,6 +15,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // Clear task event 
   clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks)
 }
 
 // Add Task
@@ -61,4 +63,18 @@ function clearTasks(){
   while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+// Filter tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
