0e8cdf89aa6c1f048c60298c2a345bc576a9b8e6
HeikoKramer <github@myforce.net>
2021-08-22 22:15:26 +0200

    remove task added

--- js_tutorials/DOM_Projects/taskList/app.js
+++ js_tutorials/DOM_Projects/taskList/app.js
@@ -11,6 +11,8 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
 }
 
 // Add Task
@@ -41,4 +43,13 @@ function addTask(e){
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
