daef03779c05ef7fbd721c286997749b2c982c6d
HeikoKramer <github@myforce.net>
2021-08-23 11:32:02 +0200

    add tasks to local storage added

--- js_tutorials/DOM_Projects/taskList/app.js
+++ js_tutorials/DOM_Projects/taskList/app.js
@@ -43,10 +43,28 @@ function addTask(e){
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in localStorage
+  storeTaskInLocalStorage(taskInput.value);
+
+
   // Clear input
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task){
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Remove Task
