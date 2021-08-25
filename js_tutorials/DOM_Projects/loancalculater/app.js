264d2ae2373acf0bc40783f6863ebbddef971fcb
HeikoKramer <github@myforce.net>
2021-08-25 11:40:53 +0200

    loan calculation math added

--- js_tutorials/DOM_Projects/loancalculater/app.js
+++ js_tutorials/DOM_Projects/loancalculater/app.js
@@ -12,5 +12,21 @@ function calculateResults(e){
   const totalPayment   = document.getElementById('total-payment');
   const totalInterest  = document.getElementById('total-interest');
 
+  const principal          = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x       = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x-1);
+
+  if(isFinite(monthly)){
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value   = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value  = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('Please check your numbers');
+  }
+
   e.preventDefault();
 }
