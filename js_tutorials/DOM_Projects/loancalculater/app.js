96dc4b0a2d407e0106b26576f5b99135226d00cf
HeikoKramer <github@myforce.net>
2021-08-25 12:36:01 +0200

    error handling added

--- js_tutorials/DOM_Projects/loancalculater/app.js
+++ js_tutorials/DOM_Projects/loancalculater/app.js
@@ -25,8 +25,35 @@ function calculateResults(e){
     totalPayment.value   = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value  = ((monthly * calculatedPayments) - principal).toFixed(2);
   } else {
-    console.log('Please check your numbers');
+    showError('Please check your numbers');
   }
 
   e.preventDefault();
+}
+
+// Show Error
+function showError(error){
+  // Create a div
+  const errorDiv      = document.createElement('div');
+
+  // Get elements
+   const card         = document.querySelector('.card');
+   const heading      = document.querySelector('.heading');
+
+  // Add class
+  errorDiv.className  = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear Error
+function clearError(){
+  document.querySelector('.alert').remove();
 }
