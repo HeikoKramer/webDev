ef4177a58cbc296bfe84437dd5b389f9822a5a86
HeikoKramer <github@myforce.net>
2021-08-26 09:35:36 +0200

    input validation and message output added

--- js_tutorials/DOM_Projects/numberguesser/app.js
+++ js_tutorials/DOM_Projects/numberguesser/app.js
@@ -24,3 +24,19 @@ const game       = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  
+  // Validate input
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}!`, 'red');
+  }
+});
+
+// Set message
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
