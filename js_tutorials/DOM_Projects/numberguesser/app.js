648a438d973b64e910b2d4d8f32b9996e92ec8e7
HeikoKramer <github@myforce.net>
2021-08-26 09:11:04 +0200

    dynamic UI min and max assignment added

--- js_tutorials/DOM_Projects/numberguesser/app.js
+++ js_tutorials/DOM_Projects/numberguesser/app.js
@@ -21,3 +21,6 @@ const game       = document.querySelector('#game'),
       guessInput = document.querySelector('#guess-input'),
       message    = document.querySelector('.message');
 
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
