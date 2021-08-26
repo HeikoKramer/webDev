19bd0d5e20b8fb03cf88a413b9201656648cf5fb
HeikoKramer <github@myforce.net>
2021-08-26 08:37:53 +0200

    game values added

---
+++ js_tutorials/DOM_Projects/numberguesser/app.js
@@ -0,0 +1,15 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+// Game values
+let min         = 1,
+    max         = 10,
+    winningNum  = 2,
+    guessesLeft = 3;
+
