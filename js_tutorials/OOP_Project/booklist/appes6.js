0966ce4fd472c59e7ba96ad799d457a0a870b8b3
HeikoKramer <github@myforce.net>
2021-08-31 13:43:55 +0200

    local storage class and methods added

--- js_tutorials/OOP_Project/booklist/appes6.js
+++ js_tutorials/OOP_Project/booklist/appes6.js
@@ -55,6 +55,54 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+      const ui = new UI;
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function(book, index) {
+      if(book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
@@ -76,6 +124,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     // Add book to list
     ui.addBookToList(book);
 
+    // Add book to local storage
+    Store.addBook(book);
+
     // Show success
     ui.showAlert('Book Added!', 'success');
 
@@ -92,10 +143,16 @@ document.getElementById('book-list').addEventListener('click', function(e) {
   // Instantiate UI
   const ui = new UI();
   
+  // Delete book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Sow alert
   ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
 })
+
+// 10:28
