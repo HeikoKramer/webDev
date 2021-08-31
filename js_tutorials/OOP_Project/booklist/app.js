73d226705fbb6b72693c46bb8a32e60ca0dac9df
HeikoKramer <github@myforce.net>
2021-08-31 11:12:00 +0200

    delete added (ES5 app completed)

--- js_tutorials/OOP_Project/booklist/app.js
+++ js_tutorials/OOP_Project/booklist/app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 2400);
 }
 
+// Delete Book
+UI.prototype.deleteBook = function(target){
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function(){
   document.getElementById('title').value  = '';
@@ -52,7 +59,7 @@ UI.prototype.clearFields = function(){
   document.getElementById('isbn').value   = '';
 }
 
-// Event Listeners
+// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
   const title   = document.getElementById('title').value;
@@ -82,3 +89,17 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 
   e.preventDefault();
 });
+
+// Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+
+  // Instantiate UI
+  const ui = new UI();
+  
+  ui.deleteBook(e.target);
+
+  // Sow alert
+  ui.showAlert('Book Removed!', 'success');
+
+  e.preventDefault();
+})
