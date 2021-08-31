b3b7355d0b96ade33c5c3053c58330cc0137b8e1
HeikoKramer <github@myforce.net>
2021-08-31 10:48:40 +0200

    validation & alerts added

--- js_tutorials/OOP_Project/booklist/app.js
+++ js_tutorials/OOP_Project/booklist/app.js
@@ -24,6 +24,27 @@ UI.prototype.addBookToList = function(book) {
   list.appendChild(row);
 }
 
+// Show Alert
+UI.prototype.showAlert = function(message, className) {
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 sec
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  }, 2400);
+}
+
 // Clear Fields
 UI.prototype.clearFields = function(){
   document.getElementById('title').value  = '';
@@ -44,11 +65,20 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   // Instantiate UI
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookToList(book);
+  // Validate
+  if(title === '' || author === '' || isbn === '') {
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
 
-  // Clear fields
-  ui.clearFields();
-  
+    // Show success
+    ui.showAlert('Book Added!', 'success');
+
+    // Clear fields
+    ui.clearFields();
+  }
+
   e.preventDefault();
 });
