2ed89c69be6e7bda1282d4729c7b9d86189ecf84
HeikoKramer <github@myforce.net>
2021-09-02 21:52:45 +0200

    plain text injection added

--- asynchronous_js.md
+++ asynchronous_js.md
@@ -169,3 +169,25 @@ xhr.onprogress = function() {
     console.log('Request Error');
   }
 ```
+
+Here the full example, how to load text from a file and inject its content into the browser window. <br>
+
+```js
+document.getElementById('button').addEventListener('click', loadData);
+
+function  loadData() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'data.txt', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+    }
+  }
+
+  xhr.send();
+}
+```
+
+![onload](/images/onload.gif)
--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,41 +1,16 @@
 document.getElementById('button').addEventListener('click', loadData);
 
 function  loadData() {
-  // Create an XHR Object
   const xhr = new XMLHttpRequest();
 
-  // Open
   xhr.open('GET', 'data.txt', true);
 
-  console.log('READYSTATE', xhr.readyState);
-  // > READYSTATE 1
-
-  // Optional – used for spinners / loaders
-  xhr.onprogress = function() {
-    console.log('READYSTATE', xhr.readyState);
-    // > READYSTATE 3
-  }
-
   xhr.onload = function() {
-    console.log(this.status);
     if(this.status === 200) {
-      console.log(this.responseText);
-      console.log('READYSTATE', xhr.readyState);
-      // > READYSTATE 4
-    } else {
-      console.log("bad request");
-      console.log('READYSTATE', xhr.readyState);
-      // > READYSTATE 4
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
     }
   }
 
-  xhr.onerror = function(){
-    console.log('READYSTATE', xhr.readyState);
-    console.log('Request Error');
-  }
-
-
-  // Send
   xhr.send();
 }
 
