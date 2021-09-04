d15e856fdf9978ead0784288b1ee4e29d625ed73
HeikoKramer <github@myforce.net>
2021-09-04 19:20:59 +0200

    first method (GET) completed

---
+++ js_tutorials/easyhttp/app.js
@@ -0,0 +1,10 @@
+const http = new easyHTTP;
+
+// Get Posts
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
---
+++ js_tutorials/easyhttp/easyhttp.js
@@ -0,0 +1,25 @@
+function easyHTTP() {
+  this.http = new XMLHttpRequest();
+}
+
+// Make a HTTP GET Request
+easyHTTP.prototype.get = function(url, callback) {
+  this.http.open('GET', url, true);
+
+  let self = this;
+  this.http.onload = function() {
+    if(self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  }
+
+  this.http.send();
+}
+
+// Make a HTTP POST Request
+
+// Make a HTTP PUT Request
+
+// Make a HTTP DELETE Request
