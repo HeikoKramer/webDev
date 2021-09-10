ce8cdfb473463c6e45fbde85c75b0c5054d7185a
HeikoKramer <github@myforce.net>
2021-09-10 10:29:00 +0200

    post request added

--- js_tutorials/easyhttp_es6/app.js
+++ js_tutorials/easyhttp_es6/app.js
@@ -1,6 +1,18 @@
 const http = new EasyHTTP;
 
 // Get Users
-http.get('https://jsonplaceholder.typicode.com/users')
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// User Data
+const data = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'some@email.com'
+}
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/users', data)
   .then(data => console.log(data))
   .catch(err => console.log(err));
--- js_tutorials/easyhttp_es6/easyhttp.js
+++ js_tutorials/easyhttp_es6/easyhttp.js
@@ -8,4 +8,20 @@ class EasyHTTP {
         .catch(err => reject(err));
     });
   }
+
+  // Make an HTTP POST Request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
 }
