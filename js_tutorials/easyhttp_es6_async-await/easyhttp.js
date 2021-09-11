6eb193ba3ac7ff98cf21c03d635ced2e60241a43
HeikoKramer <github@myforce.net>
2021-09-11 22:05:37 +0200

    async delete added

--- js_tutorials/easyhttp_es6_async-await/easyhttp.js
+++ js_tutorials/easyhttp_es6_async-await/easyhttp.js
@@ -34,22 +34,20 @@ class EasyHTTP {
 
     const resData = await response.json();
     return resData;
-
   }
 
   // Make an HTTP DELETE Request
-  delete(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Content-type' : 'application/json'
-        }
-      })
-        .then(res => res.json())
-        .then(() => resolve('Resource Deleted'))
-        .catch(err => reject(err));
+  async delete(url) {
+
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type' : 'application/json'
+      }
     });
+
+    const resData = await 'Resource Deleted';
+    return resData;
   }
 }
 
