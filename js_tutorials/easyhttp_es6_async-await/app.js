eafcf83090fd3b08c4216589008c4147b56371ad
HeikoKramer <github@myforce.net>
2021-09-11 21:58:29 +0200

    async post added

--- js_tutorials/easyhttp_es6_async-await/app.js
+++ js_tutorials/easyhttp_es6_async-await/app.js
@@ -1,9 +1,9 @@
 const http = new EasyHTTP;
 
 // Get Users
-http.get('https://jsonplaceholder.typicode.com/users')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
 // User Data
 const data = {
@@ -13,9 +13,9 @@ const data = {
 }
 
 // Create Post
-// http.post('https://jsonplaceholder.typicode.com/users', data)
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
+http.post('https://jsonplaceholder.typicode.com/users', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
 
 // Update Post
 // http.put('https://jsonplaceholder.typicode.com/users/2', data)
--- js_tutorials/easyhttp_es6_async-await/easyhttp.js
+++ js_tutorials/easyhttp_es6_async-await/easyhttp.js
@@ -7,19 +7,18 @@ class EasyHTTP {
   }
 
   // Make an HTTP POST Request
-  post(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
+  async post(url, data) {
+
+      const response = await fetch(url, {
         method: 'POST',
         headers: {
           'Content-type' : 'application/json'
         },
         body: JSON.stringify(data)
-      })
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
-    });
+      });
+
+      const resData = await response.json();
+      return resData;
   }
 
   // Make an HTTP PUT Request
