5dedf780a426aed194d11d0b5e8d16af594f5aa7
HeikoKramer <github@myforce.net>
2021-09-11 22:02:42 +0200

    async put added

--- js_tutorials/easyhttp_es6_async-await/app.js
+++ js_tutorials/easyhttp_es6_async-await/app.js
@@ -13,14 +13,14 @@ const data = {
 }
 
 // Create Post
-http.post('https://jsonplaceholder.typicode.com/users', data)
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-
-// Update Post
-// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+// http.post('https://jsonplaceholder.typicode.com/users', data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
+
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
 
 // Delete User
 // http.delete('https://jsonplaceholder.typicode.com/users/2')
--- js_tutorials/easyhttp_es6_async-await/easyhttp.js
+++ js_tutorials/easyhttp_es6_async-await/easyhttp.js
@@ -9,32 +9,32 @@ class EasyHTTP {
   // Make an HTTP POST Request
   async post(url, data) {
 
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-type' : 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
 
-      const resData = await response.json();
-      return resData;
+    const resData = await response.json();
+    return resData;
   }
 
   // Make an HTTP PUT Request
-  put(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: 'PUT',
-        headers: {
-          'Content-type' : 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
+  async put(url, data) {
+
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(data)
     });
+
+    const resData = await response.json();
+    return resData;
+
   }
 
   // Make an HTTP DELETE Request
