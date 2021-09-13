5390242150efc863d5bb5262bf07188ed4f7668f
HeikoKramer <github@myforce.net>
2021-09-13 09:39:35 +0200

    data fetch added

---
+++ js_tutorials/API_Projects/github_finder/app.js
@@ -0,0 +1,28 @@
+// Init GitHub
+const github = new GitHub;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    // Make http call
+    github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        console.log('No Profile found on GitHub');
+      } else {
+        // Show profile
+        console.log(data);
+      }
+    })
+  } else {
+    // Clear profile
+    
+  }
+});
