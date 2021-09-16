063c251aa70215a9115186d02dfda4c6c5a8d434
HeikoKramer <github@myforce.net>
2021-09-16 19:53:37 +0200

    change location added

--- js_tutorials/API_Projects/weatherjs/app.js
+++ js_tutorials/API_Projects/weatherjs/app.js
@@ -6,6 +6,19 @@ const ui = new UI();
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+
+  weather.changeLocation(city);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
+
 function getWeather() {
   weather.getWeather()
   .then(results => {
