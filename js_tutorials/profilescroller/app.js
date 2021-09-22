6b67aecb3fcec4297cb0ea9d4aca86e872c3c913
HeikoKramer <github@myforce.net>
2021-09-22 21:19:48 +0200

    data added

---
+++ js_tutorials/profilescroller/app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Boston MA',
+    image: 'https://randomuser.me/api/portraits/men/81.jpg'  
+  }
+  {
+    name: 'Jen Smith',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Raleigh NC',
+    image: 'https://randomuser.me/api/portraits/women/81.jpg'  
+  }
+  {
+    name: 'Kevin Jones',
+    age: 42,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Boston MA',
+    image: 'https://randomuser.me/api/portraits/men/20.jpg'  
+  }
+]
