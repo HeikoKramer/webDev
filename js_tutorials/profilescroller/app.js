ea8c388ee5a34431abe4da144d722b5617ffae1c
HeikoKramer <github@myforce.net>
2021-09-22 22:02:07 +0200

    profile scroller completed

--- js_tutorials/profilescroller/app.js
+++ js_tutorials/profilescroller/app.js
@@ -6,7 +6,7 @@ const data = [
     lookingfor: 'female',
     location: 'Boston MA',
     image: 'https://randomuser.me/api/portraits/men/81.jpg'  
-  }
+  },
   {
     name: 'Jen Smith',
     age: 26,
@@ -14,7 +14,7 @@ const data = [
     lookingfor: 'male',
     location: 'Raleigh NC',
     image: 'https://randomuser.me/api/portraits/women/81.jpg'  
-  }
+  },
   {
     name: 'Kevin Jones',
     age: 42,
@@ -23,4 +23,49 @@ const data = [
     location: 'Boston MA',
     image: 'https://randomuser.me/api/portraits/men/20.jpg'  
   }
-]
+];
+
+const profiles = profileIterator(data);
+
+// Call first profile
+nextProfile();
+
+// Next Event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// Next Profile Display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  if(currentProfile !== undefined) {
+    document.getElementById('profileDisplay').innerHTML = `
+      <ul class="list-group">
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+        <li class="list-group-item">Age: ${currentProfile.age}</li>
+        <li class="list-group-item">Location: ${currentProfile.location}</li>
+        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+      </ul>
+    `;
+
+    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+  } else {
+    // No more profiles
+    window.location.reload();
+
+  }
+}
+
+
+
+// Profile Iterator
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < profiles.length ? 
+      { value: profiles[nextIndex++], done: false } : 
+      { done: true }
+    }
+  }
+}
