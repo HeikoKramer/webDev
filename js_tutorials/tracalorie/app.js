a64ef4177f863240fcd214414488686e3ad6f8a2
HeikoKramer <github@myforce.net>
2021-09-27 10:42:35 +0200

    controller templates added

---
+++ js_tutorials/tracalorie/app.js
@@ -0,0 +1,17 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+  // Item Constructor
+  
+})();
+
+// UI Controller
+const UICtrl = (function() {
+
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+
+})(ItemCtrl, UICtrl);
