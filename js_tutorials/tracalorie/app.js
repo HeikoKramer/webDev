63b8f1618ebf29014526421c7d31f04a4136ad19
HeikoKramer <github@myforce.net>
2021-09-27 11:01:00 +0200

    controllers & data structure completed

--- js_tutorials/tracalorie/app.js
+++ js_tutorials/tracalorie/app.js
@@ -3,7 +3,28 @@
 // Item Controller
 const ItemCtrl = (function() {
   // Item Constructor
-  
+  const Item = function(id, name, calories) {
+    this.id       = id;
+    this.name     = name;
+    this.calories = calories;
+  }
+
+  // Data Structure / State
+  const data = {
+    items: [
+      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 1, name: 'Cookie', calories: 400},
+      {id: 2, name: 'Eggs', calories: 200},
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  return {
+    logData: function(){
+      return data;
+    }
+  }
 })();
 
 // UI Controller
@@ -14,4 +35,13 @@ const UICtrl = (function() {
 // App Controller
 const App = (function(ItemCtrl, UICtrl) {
 
+  return {
+    init: function(){
+      console.log('Initializing App');
+    }
+  }
+
 })(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init();
