0319de438af4660531294ccd6191615a5d575428
HeikoKramer <github@myforce.net>
2021-09-29 09:45:50 +0200

    item delete (from data structure) added

--- js_tutorials/tracalorie/app.js
+++ js_tutorials/tracalorie/app.js
@@ -69,8 +69,19 @@ const ItemCtrl = (function() {
           found = item;
         }
       });
-
       return found;
+    },
+    deleteItem: function(id) {
+      // Get ids
+      const ids = data.items.map(function(item) {
+        return item.id;
+      });
+
+      // Get index
+      const index = ids.indexOf(id);
+
+      // Remove item
+      data.items.splice(index, 1);
     },
     setCurrentItem: function(item) {
       data.currentItem = item;
@@ -228,7 +239,10 @@ const App = (function(ItemCtrl, UICtrl) {
     // Edit icon click event
     document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
 
-    // Update item event
+    // Delete item event
+    document.querySelector(UISelectors.deleteBtn).addEventListener('click', itemDeleteSubmit);
+
+    // Delete item event
     document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
 
     // Back button event
@@ -302,6 +316,17 @@ const App = (function(ItemCtrl, UICtrl) {
     UICtrl.showTotalCalories(totalCalories);
 
     UICtrl.clearEditState();
+
+    e.preventDefault();
+  }
+
+  // Delete item submit
+  const itemDeleteSubmit = function(e) {
+    // Get current item
+    const currentItem = ItemCtrl.getCurrentItem();
+
+    // Delete from data structure
+    ItemCtrl.deleteItem(currentItem.id);
 
     e.preventDefault();
   }
