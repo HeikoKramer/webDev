353e401a5db92919dc1a1953af433ffd763cc1fe
HeikoKramer <github@myforce.net>
2021-09-29 15:46:03 +0200

    clear local storage added

--- js_tutorials/tracalorie/app.js
+++ js_tutorials/tracalorie/app.js
@@ -50,7 +50,9 @@ const StorageCtrl = (function() {
         }
       });
       localStorage.setItem('items', JSON.stringify(items));
-
+    },
+    clearItemsFromStorage: function() {
+      localStorage.removeItem('items');
     }
   }
 })();
@@ -442,6 +444,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
     // Remove from UI
     UICtrl.removeItems();
+
+    // Clear local storage
+    StorageCtrl.clearItemsFromStorage();
 
     // Hide UL
     UICtrl.hideList();
