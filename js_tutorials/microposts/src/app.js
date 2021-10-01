773e62a21aab2d5953416549ddc43d1bfe4f6279
HeikoKramer <github@myforce.net>
2021-10-01 14:00:16 +0200

    delete post added

--- js_tutorials/microposts/src/app.js
+++ js_tutorials/microposts/src/app.js
@@ -7,6 +7,12 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 // Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -32,4 +38,28 @@ function submitPost() {
       getPosts();
     })
     .catch(err => console.log(err));
+}
+
+// Delete Post
+function deletePost(e) {
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+  e.preventDefault();
+}
+
+// Enable edit
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    console.log(e.target.parentElement.dataset.id);
+  }
+  e.preventDefault();
 }
