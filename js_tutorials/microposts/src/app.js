5a1794cc41cce2fe0bea449bc5d0143864a5ced4
HeikoKramer <github@myforce.net>
2021-10-01 12:01:35 +0200

    add post & show alert added

--- js_tutorials/microposts/src/app.js
+++ js_tutorials/microposts/src/app.js
@@ -4,8 +4,32 @@ import { ui }   from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
   .catch(err => console.log(err));
+}
+
+// Submit Posts
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body  = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
 }
