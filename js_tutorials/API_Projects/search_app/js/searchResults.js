dcc9b5c8c5188a9f677cb9a6a3c560acbc288dd2
HeikoKramer <github@myforce.net>
2021-10-13 13:59:31 +0200

    createResultItem added

--- js_tutorials/API_Projects/search_app/js/searchResults.js
+++ js_tutorials/API_Projects/search_app/js/searchResults.js
@@ -15,3 +15,16 @@ export const buildSearchResults = (resultArray) => {
   });
 }
 
+const createResultItem = (result) => {
+  const resultItem = document.createElement("div");
+  resultItem.classList.add("resultItem");
+  const resultTitle = document.createElement("div");
+  resultTitle.classList.add("resultTitle");
+  const link = document.createElement("a");
+  link.href = `https://en.wikipedia.org/?curid=${result.id}`;
+  link.textContent = result.title;
+  link.target = "_blank";
+  resultTitle.append(link);
+  resultItem.append(resultTitle);
+  return resultItem;
+}
