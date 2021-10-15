4f941a2b6cc1e9eaf28645095d9f193353b76650
HeikoKramer <github@myforce.net>
2021-10-15 13:47:38 +0200

    checkOrder added, tutorial completed

--- js_tutorials/drag-n-drop/script.js
+++ js_tutorials/drag-n-drop/script.js
@@ -82,6 +82,19 @@ function swapItems(fromIndex, toIndex) {
   listItems[toIndex].appendChild(itemOne);
 }
 
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector('.draggable').innerText.trim();
+
+    if(personName !== richestPeople[index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
+}
+
 function addEventListeners() {
   const draggables = document.querySelectorAll('.draggable');
   const dragListItems = document.querySelectorAll('.draggable-list li');
@@ -97,3 +110,5 @@ function addEventListeners() {
     item.addEventListener('dragleave', dragLeave);
   });
 }
+
+check.addEventListener('click', checkOrder);
