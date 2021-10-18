73e5b0a8b2313d09d4a2408f961e932860e1154d
HeikoKramer <github@myforce.net>
2021-10-18 13:19:16 +0200

    option and hiddenVal randomisers added

--- js_tutorials/math_game/app.js
+++ js_tutorials/math_game/app.js
@@ -9,7 +9,7 @@ gameArea.append(output);
 gameArea.append(btn);
 
 const opts = ['*','/','+','-'];
-const game = {maxValue:10,questions:10,oper:[2],curQue:0};
+const game = {maxValue:10,questions:10,oVals:[0,2,3],curQue:0};
 
 btn.addEventListener('click', startGame);
 
@@ -26,8 +26,14 @@ function buildQuestion() {
     let vals = [];
     vals[0] = Math.floor(Math.random()*(game.maxValue+1));
     vals[1] = Math.floor(Math.random()*(game.maxValue+1));
-    vals[2] = eval(vals[0] + opts[2] + vals[1]);
-    output.innerHTML = `${vals[0]} ${opts[2]} ${vals[1]} = ${vals[2]}`;
+    game.oVals.sort(()=>{return 0.5 - Math.random()});
+    vals[3] = opts[game.oVals[0]];
+    console.log(game.oVals);
+    vals[2] = eval(vals[0] + vals[3] + vals[1]);
+    let hiddenVal = Math.floor(Math.random()*3);
+    console.log('hiddenVal: ' + hiddenVal);
+    vals[hiddenVal] = '––';
+    output.innerHTML = `${vals[0]} ${vals[3]} ${vals[1]} = ${vals[2]}`;
   }
   
 }
