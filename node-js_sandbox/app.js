145ea5034500817b5bb1cc2f31d5ab8d58790f4c
HeikoKramer <github@myforce.net>
2021-11-15 19:13:34 +0100

    map added

--- node-js_sandbox/app.js
+++ node-js_sandbox/app.js
@@ -1,29 +1,17 @@
 'use strict';
 
-const processArray = function (items, fn) {
-  const result = [];
-
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-
-    result.push(fn(item));
-  }
-
-  return result;
-};
-
 const numbers = [ 1, 2, 3, 4, 5 ];
 
-const squares = processArray(numbers, n => n ** 2);
+const squares = numbers.map(n => n ** 2);
 console.log('squares:', squares);
 
-const numsPlusOne = processArray(numbers, n => n + 1);
+const numsPlusOne = numbers.map(n => n + 1);
 console.log('numsPlusOne:', numsPlusOne);
 
 const words = [ 'the', 'quick', 'brown', 'fox' ];
-const wordObjects = processArray(words, w => ({ word: w, length: w.length}));
+const wordObjects = words.map(w => ({ word: w, length: w.length}));
 console.log('wordObjects:', wordObjects);
 
-const wordLengths = processArray(words, w => w.length);
+const wordLengths = words.map(w => w.length);
 console.log('wordLengths:', wordLengths);
 
--- nodeJS_notes.md
+++ nodeJS_notes.md
@@ -1981,3 +1981,28 @@ console.log('wordObjects:', wordObjects);
 ```
 
 ![wordObjects-output](/images/wordObjects-output.png)
+
+### array.map()
+Our example from above is even easier to process with the array method `map()`. <br>
+Map is usable on an array, takes in a function parameter and applies that function to each value of the array. <br>
+
+```js
+'use strict';
+
+const numbers = [ 1, 2, 3, 4, 5 ];
+
+const squares = numbers.map(n => n ** 2);
+console.log('squares:', squares);
+
+const numsPlusOne = numbers.map(n => n + 1);
+console.log('numsPlusOne:', numsPlusOne);
+
+const words = [ 'the', 'quick', 'brown', 'fox' ];
+const wordObjects = words.map(w => ({ word: w, length: w.length}));
+console.log('wordObjects:', wordObjects);
+
+const wordLengths = words.map(w => w.length);
+console.log('wordLengths:', wordLengths);
+```
+
+![array-map](/images/array-map.png)
