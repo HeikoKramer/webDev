9eef3c7528a507d5f0ec94db9cc99b0c76031d4e
HeikoKramer <github@myforce.net>
2021-11-21 18:46:05 +0100

    http methods and rest apis completed

--- node-js_sandbox/server.js
+++ node-js_sandbox/server.js
@@ -18,21 +18,47 @@ const nullResponse = JSON.stringify({
 });
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-    'X-Powered-By': 'Node.js'
-  });
-
+  const { method, url } = req;
   let body = [];
 
   req.on('data', chunk => {
     body.push(chunk);
   }).on('end', () => {
     body = Buffer.concat(body).toString();
-    console.log(body);
-  });
 
-  res.end(successfullResponse);
+    let status = 404;
+    const response = {
+      success: false,
+      data: null,
+      error: null
+    }
+
+    if(method === 'GET' && url === '/todos') {
+      status = 200;
+      response.success = true;
+      response.data = todos;
+    } else if(method === 'POST' && url === '/todos') {
+      const { id, text } = JSON.parse(body);
+
+      if(!id || !text) {
+        status = 400;
+        response.error = 'please add id and text';
+      } else {
+        todos.push({ id, text });
+        status = 201;
+        response.success = true;
+        response.data = todos;
+      }
+    }
+    
+    res.writeHead(status, {
+      'Content-Type': 'application/json',
+      'X-Powered-By': 'Node.js'
+    });
+
+    res.end(JSON.stringify(response));
+  });
+ 
 });
 
 const PORT = 5000;
