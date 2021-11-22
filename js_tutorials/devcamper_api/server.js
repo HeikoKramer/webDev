7e9fb9ecaed4fd138acf55cf2c1e62941f93dfdd
HeikoKramer <github@myforce.net>
2021-11-22 21:48:45 +0100

    bootcamps routes added

--- js_tutorials/devcamper_api/server.js
+++ js_tutorials/devcamper_api/server.js
@@ -6,9 +6,29 @@ dotenv.config({ path: './config/convig.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT, 
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-  
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
