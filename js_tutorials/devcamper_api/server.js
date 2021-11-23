b778790e91c4db613307fa2b76bb2242f08a7ff2
HeikoKramer <github@myforce.net>
2021-11-23 10:21:21 +0100

    bootcamps routes outsourced

--- js_tutorials/devcamper_api/server.js
+++ js_tutorials/devcamper_api/server.js
@@ -1,30 +1,17 @@
 const express = require('express');
 const dotenv  = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 //  Load env vars
 dotenv.config({ path: './config/convig.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
 
 const PORT = process.env.PORT || 5000;
 
