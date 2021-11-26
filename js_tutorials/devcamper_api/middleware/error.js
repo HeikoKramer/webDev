f44f37453e228bddbe87410d86cfd09544ef43d9
HeikoKramer <github@myforce.net>
2021-11-26 19:20:31 +0100

    error validation added

--- js_tutorials/devcamper_api/middleware/error.js
+++ js_tutorials/devcamper_api/middleware/error.js
@@ -6,12 +6,26 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for dev
-  console.log(err.stack.bold.black.bgRed);
+  console.log('ERRORNAME:', err.name);
+  console.log('ERRORCODE:', err.code);
+  console.log('ERROR:', err);
 
   // Mongose bad ObjectId
   if(err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if(err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose ValidatonError
+  if(err.name === 'ValidationError') {
+    const message = 'Please add required fields: ' + Object.values(err.errors).map(val => val.path);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
