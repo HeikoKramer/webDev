fb1f1b1e6cbacc4210489bf7e81a6dcef91915ba
HeikoKramer <github@myforce.net>
2021-11-28 15:42:35 +0100

    pagination prev & next added

--- js_tutorials/devcamper_api/controllers/bootcamps.js
+++ js_tutorials/devcamper_api/controllers/bootcamps.js
@@ -43,18 +43,38 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   }
 
   // Pagination
-  const page  = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 100; 
-  const skip  = (page - 1) * limit;
+  const page        = parseInt(req.query.page, 10) || 1;
+  const limit       = parseInt(req.query.limit, 10) || 25; 
+  const startIndex  = (page - 1) * limit;
+  const endIndex    = page * limit;
+  const total       = await Bootcamp.countDocuments();
 
-  query = query.skip(skip).limit(limit);
+  query = query.skip(startIndex).limit(limit);
+
+  // Pagination result
+  const pagination = {};
+
+  if(endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if(startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
 
   // Executing resource
   const bootcamps = await Bootcamp.find(query);
 
   res.status(200).json({ 
     success: true, 
-    count: bootcamps.length, 
+    count: bootcamps.length,
+    pagination, 
     data: bootcamps
   });
 });
