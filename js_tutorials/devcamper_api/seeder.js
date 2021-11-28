bc97d43a55da57846be06463c407183b5fc3ad57
HeikoKramer <github@myforce.net>
2021-11-28 21:53:03 +0100

    courses added to seeder, courses data fixed

--- js_tutorials/devcamper_api/seeder.js
+++ js_tutorials/devcamper_api/seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course   = require('./models/Course');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,11 +21,16 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+// Read JSON files
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import into database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data Imported'.green.inverse);
     process.exit();
   } catch (err) {
@@ -36,6 +42,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Destroyed'.red.inverse);
     process.exit();
   } catch (err) {
