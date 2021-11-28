6175316dda28231679e589f2bc4d144757fbfb5b
HeikoKramer <github@myforce.net>
2021-11-28 10:51:57 +0100

    field selector logic added

--- js_tutorials/devcamper_api/controllers/bootcamps.js
+++ js_tutorials/devcamper_api/controllers/bootcamps.js
@@ -7,15 +7,33 @@ const Bootcamp = require('../models/Bootcamp');
 //  @route   GET /api/v1/bootcamps
 //  @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-
   let query;
 
-  let queryStr = JSON.stringify(req.query);
+  // Copy req.query
+  const reqQuery = { ...req.query };
 
+  // Fields to exclude
+  const removeFields = ['select'];
+
+  //  Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
+  // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  // Select fields
+  if(req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Executing resource
   const bootcamps = await Bootcamp.find(query);
 
   res.status(200).json({ 
