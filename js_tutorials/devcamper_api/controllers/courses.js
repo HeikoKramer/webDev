c37bc39f14df84e4444a1e1c9c1cc58f1fea1a27
HeikoKramer <github@myforce.net>
2021-11-29 22:37:17 +0100

    get single course added

--- js_tutorials/devcamper_api/controllers/courses.js
+++ js_tutorials/devcamper_api/controllers/courses.js
@@ -26,3 +26,22 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     data: courses
   });
 });
+
+//  @desc    Get single course
+//  @route   GET /api/v1/courses(:id)
+//  @access  Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: ' name description'
+  });
+
+  if(!course) {
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
--- js_tutorials/devcamper_api/routes/courses.js
+++ js_tutorials/devcamper_api/routes/courses.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { 
-  getCourses
+  getCourses,
+  getCourse
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
 
