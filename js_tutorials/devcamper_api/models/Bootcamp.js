ad8013895681e5abaae7d048ad2b734e533c3a61
HeikoKramer <github@myforce.net>
2021-11-29 19:06:37 +0100

    cascade delete added, bootcamp -> courses

--- js_tutorials/devcamper_api/models/Bootcamp.js
+++ js_tutorials/devcamper_api/models/Bootcamp.js
@@ -132,6 +132,13 @@ BootcampSchema.pre('save', async function(next) {
   next();
 });
 
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
+
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
