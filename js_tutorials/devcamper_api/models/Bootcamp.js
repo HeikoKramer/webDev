169f8d1921af09a86f51de9ab986a21117106a4d
HeikoKramer <github@myforce.net>
2021-11-29 18:52:34 +0100

    courses added to bootcamp (with virtuals)

--- js_tutorials/devcamper_api/models/Bootcamp.js
+++ js_tutorials/devcamper_api/models/Bootcamp.js
@@ -100,6 +100,9 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 // Create bootcamp slug from the name
@@ -128,5 +131,13 @@ BootcampSchema.pre('save', async function(next) {
   this.address = undefined;
   next();
 });
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
