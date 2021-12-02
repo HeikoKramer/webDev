2031fe06217241402ed12dd6b1de79a156ae25fb
HeikoKramer <github@myforce.net>
2021-12-02 20:14:36 +0100

    bootcamp controller methods outsourced to middleware

--- js_tutorials/devcamper_api/routes/bootcamps.js
+++ js_tutorials/devcamper_api/routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
@@ -25,7 +28,7 @@ router.route('/:id/photo')
 
 router
   .route('/')
-  .get(getBootcamps)
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
   .post(createBootcamp);
 
 router
