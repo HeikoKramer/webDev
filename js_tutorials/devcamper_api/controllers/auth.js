0faefec6b04ae0d1821a1062150f78bebff801ea
HeikoKramer <github@myforce.net>
2021-12-04 17:43:40 +0100

    user login added

--- js_tutorials/devcamper_api/controllers/auth.js
+++ js_tutorials/devcamper_api/controllers/auth.js
@@ -21,3 +21,34 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token });
 });
+
+//  @desc    Login user
+//  @route   POST /api/v1/auth/login
+//  @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email & password
+  if(!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  //  Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if(!user) {
+    return next(new ErrorResponse('Invalid credantials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if(!isMatch) {
+    return next(new ErrorResponse('Invalid credantials', 401));
+  };
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
