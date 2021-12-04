5537f4cab0055e5669716da503be069782452a75
HeikoKramer <github@myforce.net>
2021-12-04 14:21:01 +0100

    register user added

--- js_tutorials/devcamper_api/controllers/auth.js
+++ js_tutorials/devcamper_api/controllers/auth.js
@@ -3,8 +3,18 @@ const asyncHandler  = require('../middleware/async');
 const User          = require('../models/User');
 
 //  @desc    Register user
-//  @route   GET /api/v1/auth/register
+//  @route   POST /api/v1/auth/register
 //  @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  res.status(200).json({ success: true});
 });
