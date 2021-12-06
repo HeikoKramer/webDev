bed2a3e72d5744c973924103da21cc5d3e060dde
HeikoKramer <github@myforce.net>
2021-12-06 21:33:28 +0100

    users added to seeder

--- js_tutorials/devcamper_api/seeder.js
+++ js_tutorials/devcamper_api/seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course   = require('./models/Course');
+const User   = require('./models/User');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,11 +27,17 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+// Read JSON files
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 // Import into database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log('Data Imported'.green.inverse);
     process.exit();
   } catch (err) {
@@ -43,6 +50,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log('Data Destroyed'.red.inverse);
     process.exit();
   } catch (err) {
