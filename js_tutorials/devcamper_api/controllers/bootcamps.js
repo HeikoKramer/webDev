ac9e89581ee553ceaa6edb7cdad4412aa0534022
HeikoKramer <github@myforce.net>
2021-12-08 21:16:36 +0100

    update restricted to owner and admin

--- js_tutorials/devcamper_api/controllers/bootcamps.js
+++ js_tutorials/devcamper_api/controllers/bootcamps.js
@@ -56,16 +56,25 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 //  @route   PUT /api/v1/bootcamps/:id
 //  @access  Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
   if(!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // Make sure user is bootcamp owner
+  if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User ${req.params.id} is not autorized to update this bootcamp`, 404)
+    );
+  }
+
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
   res.status(200).json({ 
     success: true, 
