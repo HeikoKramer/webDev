973a281a29129c3b7680f8f1dd662fb24f17bca2
HeikoKramer <github@myforce.net>
2021-12-08 21:45:31 +0100

    course creation restricted to bootcamp owner or admin

--- js_tutorials/devcamper_api/controllers/courses.js
+++ js_tutorials/devcamper_api/controllers/courses.js
@@ -47,6 +47,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 //  @access  Public
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user     = req.user.id;
 
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
   
@@ -54,6 +55,13 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     return next(
       new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 
       404
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User ${req.user.id} is not autorized to add a course to bootcamp ${bootcamp._id}.`, 401)
     );
   }
 
