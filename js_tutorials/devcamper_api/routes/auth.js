201730eb6bf51af7a17b9e6f77d9dc54df2d45ed
HeikoKramer <github@myforce.net>
2021-12-11 21:45:33 +0100

    update user name and email added

--- js_tutorials/devcamper_api/routes/auth.js
+++ js_tutorials/devcamper_api/routes/auth.js
@@ -4,7 +4,8 @@ const {
   login, 
   getMe,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updateDetails
 } = require('../controllers/auth');
 
 
@@ -15,6 +16,7 @@ const { protect } = require('../middleware/auth')
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
