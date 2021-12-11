bc53fa020bad2ceb51b4fea367b33d26aa1d54db
HeikoKramer <github@myforce.net>
2021-12-11 18:39:45 +0100

    reset password completed

--- js_tutorials/devcamper_api/routes/auth.js
+++ js_tutorials/devcamper_api/routes/auth.js
@@ -3,7 +3,8 @@ const {
   register, 
   login, 
   getMe,
-  forgotPassword 
+  forgotPassword,
+  resetPassword
 } = require('../controllers/auth');
 
 
@@ -15,5 +16,6 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
