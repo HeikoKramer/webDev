0a9d89828abfd9deb9f87d8bf2a9c585faf2f111
HeikoKramer <github@myforce.net>
2021-12-12 11:01:05 +0100

    user router added

--- js_tutorials/devcamper_api/routes/courses.js
+++ js_tutorials/devcamper_api/routes/courses.js
@@ -7,11 +7,11 @@ const {
   deleteCourse
 } = require('../controllers/courses');
 
-const Course          = require('../models/Course');
-const advancedResults = require('../middleware/advancedResults');
+const Course = require('../models/Course');
 
 const router = express.Router({ mergeParams: true });
 
+const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 router
