d6281d57b139ac1f78ef1f4c450a56ea59f86919
HeikoKramer <github@myforce.net>
2021-12-12 10:43:00 +0100

    update user route added

--- js_tutorials/devcamper_api/controllers/users.js
+++ js_tutorials/devcamper_api/controllers/users.js
@@ -32,3 +32,18 @@ exports.createUser = asyncHandler(async (req, res, next) => {
     data: user
   });
 });
+
+//  @desc    Update user
+//  @route   PUT /api/v1/auth/users/:id
+//  @access  Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
