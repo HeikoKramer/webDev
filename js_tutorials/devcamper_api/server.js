3abb763505f20163eb94f8db54769b74c06bffbc
HeikoKramer <github@myforce.net>
2021-12-14 22:54:15 +0100

    xss-clean added to prevent XSS attacks

--- js_tutorials/devcamper_api/server.js
+++ js_tutorials/devcamper_api/server.js
@@ -7,6 +7,7 @@ const fileupload    = require('express-fileupload');
 const cookieParser  = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet        = require('helmet');
+const xss           = require('xss-clean');
 const errorHandler  = require('./middleware/error');
 const connectDB     = require('./config/db');
 
@@ -44,6 +45,9 @@ app.use(mongoSanitize());
 
 // Set security headers
 app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
