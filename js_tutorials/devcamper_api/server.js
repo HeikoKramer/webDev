fa28fcb4af8626cc50fe9c50c53a842ae4221a6c
HeikoKramer <github@myforce.net>
2021-12-14 23:09:24 +0100

    express-rate-limit and hpp added

--- js_tutorials/devcamper_api/server.js
+++ js_tutorials/devcamper_api/server.js
@@ -8,6 +8,8 @@ const cookieParser  = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet        = require('helmet');
 const xss           = require('xss-clean');
+const rateLimit     = require('express-rate-limit');
+const hpp           = require('hpp');
 const errorHandler  = require('./middleware/error');
 const connectDB     = require('./config/db');
 
@@ -51,6 +53,17 @@ app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // set timeframe of 10 mins
+  max: 100                  // limit amount of possible requests during that time
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
